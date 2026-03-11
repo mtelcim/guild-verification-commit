e4651f4d8232cb9371bc44be5841a29e60e1f48c
mtelcim <166520617+mtelcim@users.noreply.github.com>
2026-03-11 15:45:24 +0300

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-11T12:45:07.263Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-11T12:45:25.759Z
+Commit: Fix CSS styling bug on mobile devices
