184841015c3744fc83ba28b94ef42b8b9ba8e22a
mtelcim <166520617+mtelcim@users.noreply.github.com>
2026-03-11 15:34:41 +0300

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-11T12:34:30.687Z
-Commit: Add multi-language support
+Timestamp: 2026-03-11T12:34:41.850Z
+Commit: Resolve null pointer exception in user service
