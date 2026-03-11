6aa042dd21d482f4ff4c9b40e20f3e0d71f19116
mtelcim <166520617+mtelcim@users.noreply.github.com>
2026-03-11 15:45:13 +0300

    Fix broken links in documentation

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-11T12:45:10.697Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-11T12:45:13.816Z
+Commit: Fix broken links in documentation
